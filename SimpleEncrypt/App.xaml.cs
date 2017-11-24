f46726faa263c7be02f7eb90867463d6449a35ed
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-11-25 01:45:19 +0200

    Console mode
    To use program you can also drag file into program

--- SimpleEncrypt/App.xaml.cs
+++ SimpleEncrypt/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,10 +14,42 @@ namespace SimpleEncrypt
   /// </summary>
   public partial class App : Application
   {
+    [DllImport("Kernel32")]
+    public static extern void AllocConsole();
+
+    [DllImport("Kernel32")]
+    public static extern void FreeConsole();
     private void App_OnStartup(object sender, StartupEventArgs e)
     {
       var argsCount = e.Args.Length;
-      
+      if (argsCount > 0)
+      {
+        var path = e.Args[0];
+        AllocConsole();
+        Encryptor encryptor = new Encryptor();
+
+        encryptor.EncryptionProgressEvent += delegate (float progress, int type)
+        {
+          Console.SetCursorPosition(0, 1);
+          Console.Write(new String(' ', Console.BufferWidth));
+          if (type == 0)
+            Console.Write($"Encryption progress {(int)progress} %");
+          else
+            Console.Write($"Decryption progress {(int)progress} %");
+        };
+        if (path.Contains(".encr"))
+        {
+          Console.WriteLine($"File to decrypt: {path}");
+          encryptor.DecryptFile(path);
+        }
+        else
+        {
+          Console.WriteLine($"File to encrypt: {path}");
+          encryptor.EncryptFile(path);
+        }
+        Console.ReadKey();
+        Application.Current.Shutdown();
+      }
     }
   }
 }
