71bba6c6f53681cf05b0f311c76d22079066e639
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-11-24 22:24:04 +0200

    Add key property

---
+++ SimpleEncrypt/KeyGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEncrypt
+{
+  public class KeyGeneration
+  {
+
+    public string Key => GetHardDrivesInfo();
+
+    private string GetHardDrivesInfo()
+    {
+      ManagementObjectSearcher moSearcher = 
+        new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+      string no = "";
+      foreach (var mo in moSearcher.Get())
+      {
+        no +=mo["SerialNumber"].ToString();
+      }
+
+      return no;
+    }
+  }
+}
