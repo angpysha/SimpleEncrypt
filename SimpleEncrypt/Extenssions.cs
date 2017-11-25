e82552cf4e39880b311b5be02dcf48680ddf86e6
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2017-11-25 22:04:19 +0200

    Extensions method

---
+++ SimpleEncrypt/Extenssions.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleEncrypt
+{
+  public static class Extenssions
+  {
+    public static string GetFileExtension(this string filepath)
+    {
+      var arr = filepath.Split('.');
+
+      return arr[1];
+    }
+  }
+}
